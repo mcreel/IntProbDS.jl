53bc52dd49ea6518604ddef3922e246f7af702e5
Michael Creel <michael.creel@uab.es>
2021-10-12 12:18:33 +0200

    ch1 done

--- Ch1.jl
+++ Ch1.jl
@@ -1,24 +1,51 @@
-println("Chapter 1.1")
-println("visualizing a geometric series")
+# Chapter 1.1
+
+# Visualizing a geometric series
+
+## Julia code to generate a geometric series
 using StatsPlots
 p = 0.5
 n = 1:10
 X = p .^ n
 bar(n, X, legend=false)
 
-println("compute N choose K")
+## compute N choose K
+# Julia code to (N choose K) and K!
 n = 10
 k = 2
-@show binomial(n, k)
-@show factorial(k)
+binomial(n, k)
+factorial(k)
 
-println("Chapter 1.4")
-println("Inner product of two vectors")
+# Chapter 1.4
+
+# Inner product of two vectors
+
+## Julia code to perform and inner product
 using LinearAlgebra
 x = [1, 0, -1]
 y = [3, 2, 0]
-@show dot(x,y)
-println("Norm of a vector")
+x'y
+
+## Norm of a vector
+# Julia code to compute the norm
+using LinearAlgebra
 x = [1, 0, -1]
-@show norm(x)
-nothing
+norm(x)
+
+## Weighted norm of a vector
+# Julia code to compute the weighted norm
+W = [1. 2. 3.; 4. 5. 6.; 7. 8. 9.]
+x = [2, -1, 1]
+z = x'W*x
+
+## Matrix inverse
+# Julia code to compute a matrix inverse
+X = [1. 3.; -2. 7.; 0. 1.]
+XtX = X'X
+XtXinv = inv(XtX)
+
+## System of linear equations
+# Julia code to solve Xβ = y
+X = [1. 3.; -2. 7.; 0. 1.]
+y = [2., 1., 0.]
+β = X\y
