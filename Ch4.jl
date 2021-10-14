8fa298c19f991c2a3764c96f6e21be21179e97f3
Rodrigo Chang <rrcp777@gmail.com>
2021-10-14 11:20:20 -0600

    Removed MATLAB style

--- Ch4.jl
+++ Ch4.jl
@@ -8,19 +8,6 @@ using Plots
 
 gmm = MixtureModel(Normal[Normal(0, 1), Normal(5, 1),], [0.3, 0.7])
 
-# x = range(-5, 10, length=1000)
-# f = pdf(gmm, x)
-
-# x1 = range(-5, 1, length=1000)
-# f1 = pdf(gmm, x1)
-
-# p1 = plot(x1, zeros(1000), fillrange=f1, 
-#     linealpha=0, fillcolor=RGB(0.8, 0.8, 1), alpha=0.5, label=false)
-# plot!(p1, x -> pdf(gmm, x), -5, 10, label="PDF", linewidth=6, color=1)
-
-# F = cdf(gmm, x)
-# p2 = plot(x, F, linewidth=6, label="CDF")
-
 p1 = plot(x -> 0, -5, 1, fillrange= x->pdf(gmm, x), 
     linealpha=0, fillcolor=RGB(0.8, 0.8, 1), alpha=0.5, label=false) # style
 plot!(p1, x -> pdf(gmm, x), -5, 10, 
