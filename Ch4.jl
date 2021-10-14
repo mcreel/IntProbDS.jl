833027fe34f3541a8f8196d72ab14902519d8096
Rodrigo Chang <rrcp777@gmail.com>
2021-10-14 16:36:00 -0600

    Uniform examples

--- Ch4.jl
+++ Ch4.jl
@@ -3,7 +3,7 @@
 # CDF of a Gaussian mixture
 
 ## Julia code to generate the PDF and CDF
-using Distributions: Normal, MixtureModel
+using Distributions
 using Plots
 
 gmm = MixtureModel(Normal[Normal(0, 1), Normal(5, 1),], [0.3, 0.7])
@@ -20,8 +20,11 @@ plot(p1, p2, layout=(1, 2),
     size=(600, 300), legend=:topleft)
 
 
-
+##
 # CDF of a uniform random variable
+using Distributions
+using Plots
+
 u = Uniform(-3, 4)
 
 p1 = plot(x -> 0, -5:0.01:1, fillrange=x->pdf(u, x),
@@ -36,3 +39,57 @@ vline!(p2, [-3, 4],
 
 plot(p1, p2, layout=(1, 2), 
     size=(600, 300), legend=:topleft)
+
+## 
+# CDF of an exponential random variable
+using Distributions
+using Plots
+
+u = Exponential(2)
+
+p1 = plot(x -> 0, -5:0.01:1, fillrange=x->pdf(u, x),
+    linealpha=0, fillcolor=RGB(0.8, 0.8, 1), alpha=0.5, label=false)
+plot!(p1, x -> pdf(u, x), -5, 10, 
+    color=1, linewidth=6, ylims=(0, 0.6), label="PDF")
+
+p2 = plot(x -> cdf(u, x), -5:0.01:10, 
+    linewidth=6, label="CDF")
+
+plot(p1, p2, layout=(1, 2), 
+    size=(600, 300), legend=:topleft)
+
+
+# Chapter 4.5
+
+# Generate a uniform random variable
+using Distributions
+using Plots
+
+u = Uniform(0, 1) # same as Uniform()
+X = rand(u, 1000)
+histogram(X)
+
+
+
+# Mean, variance, median, mode of a uniform random variable
+
+# Julia code to compute empirical mean, var, median, mode
+using Distributions
+u = Uniform(0, 1)
+X = rand(u, 1000)
+
+M = mean(X)
+V = var(X)
+Med = median(X)
+Mod = mode(X)
+
+
+# Probability of a uniform random variable
+
+# Julia code to compute the probability P(0.2 < X < 0.3)
+using Distributions
+
+u = Uniform(0, 1)
+F = cdf(u, 0.3) - cdf(u, 0.2)
+
+
